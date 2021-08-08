5d090b0289cc0167aa2b3f624d0d0e4330795912
hhzule <hhzule@gmail.com>
2021-08-09 02:26:03 +0500

    timestamp 1:12:06

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
             <Button
-            color="blue"
+            color="gray"
             buttonType="outline"
             rounded={true}
             iconOnly={true} 
@@ -15,6 +15,31 @@ const Header = () => {
             <Icon name="description" size="5xl" color="blue"/>
             <h1 className="md:inline-flex ml-2 text-gray-700 text-2xl">
                 DOCS</h1>
+
+                <div className="md:mx-20 mx-5 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+                    <Icon name="search" size="3xl" color="gray"/>
+               <input 
+               className="flex-grow px-5 text-base bg-transparent outline-none"
+               type="text" placeholder="Search"/>
+                </div>
+                <Button
+            color="gray"
+            buttonType="outline"
+            rounded={true}
+            iconOnly={true} 
+            ripple="dark"
+            className="md:ml-20 ml-5 h-20 w-20 border-0"
+            >
+<Icon name="apps" size="3xl" color="gray"/>
+
+            </Button>
+<img 
+// onClick={} 
+className="cursor-pointer h-12 w-12 rounded-full ml-2"
+src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"
+/>
+
+
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import Button from "@material-tailwind/react/Button"
+import Icon from "@material-tailwind/react/Icon";
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -12,7 +15,37 @@ export default function Home() {
       </Head>
 
      <Header/>
+<section  className="bg-[#F8F9FA] pb-10 -x-10">
+  <div  className="max-w-3xl mx-auto" >
+    <div  className="flex items-center justify-between py-6">
+      <h2  className="text-gray-700 text-lg"> Start a new document</h2>
+      <Button
+            color="gray"
+            buttonType="outline"
+            rounded={true}
+            iconOnly={true} 
+            ripple="dark"
+            className="border-0"
+            >
+<Icon name="more_vert" size="3xl" color="gray"/>
 
+            </Button>
+  
+    </div>
+<div>
+ <div className="relative h-52 w-40">
+
+<Image 
+src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"
+
+layout="fill"/>
+   
+   </div>
+</div>
+  </div>
+
+
+</section>
       <footer className="flex items-center justify-center w-full h-24 border-t">
   footer
       </footer>
